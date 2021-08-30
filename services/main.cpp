d58de0f2698e29f03d447b830664f4f7847e8271
nathan <tengfei_work@163.com>
2021-08-30 17:06:12 +0800

    epoll test

--- services/main.cpp
+++ services/main.cpp
@@ -1,6 +1,9 @@
 #include "threadpool.hpp"
+#include "tcpserver.h"
 #include "task.h"
 int main(){
     threadpool<task>* pool = new threadpool<task>;
+    tcpserver* server = new tcpserver(pool);
+    server->connect();
     return 0;
 }
